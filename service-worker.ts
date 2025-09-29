700b689f87434b89f04f7caf47b68ca4068c258f
UnknownMemory <jeremyschneider21@gmail.com>
2025-09-29 17:44:06 +0200

    add service worker

---
+++ service-worker.ts
@@ -0,0 +1,43 @@
+declare let self: ServiceWorkerGlobalScope
+
+const cacheName = "audio-trimmer-v1";
+const assets = ["index.html", "DJ YARI - EVERYTHING IS BUSINES PT.2.m4a", "icon.svg", "assets/index.js", "assets/index.css"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      cache.addAll(assets);
+    })()
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys()
+
+      keys.forEach(async (key) => {
+        if (key !== cacheName) {
+          await caches.delete(key)
+        }
+      })
+    })()
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.startsWith("chrome-extension")) return;
+
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      let response = await cache.match(event.request.url);
+      if (response === undefined) {
+        response = await fetch(event.request.url);
+        cache.put(event.request.url, response.clone());
+      }
+      return response
+    })()
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/media-trimmer/",
-  plugins: [react()]
+  plugins: [react(), {
+    name: "transform-sw-path",
+    apply: "build",
+    transformIndexHtml(html) {
+      return html.replace("service-worker.ts", "service-worker.js")
+    }
+  }],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "./index.html",
+        'service-worker': './service-worker.js'
+      },
+      output: {
+        entryFileNames: asset => asset.name == "service-worker" ? '[name].js' : 'assets/[name].js',
+        assetFileNames: "assets/[name].[ext]"
+      }
+    }
+  }
 })
